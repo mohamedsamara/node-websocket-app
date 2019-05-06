35103a9d6985fa10d7c962304c9c5c0d1420532c
Mohamed Samara <45723974+mohamedsamara@users.noreply.github.com>
2019-05-06 00:39:34 -0700

    setup mongoose & mongodb atlas connection

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const path = require('path');
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+const db = require('./app/config/keys').mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log('MongoDB Connected!'))
+  .catch(err => console.log(err));
+
+// set public folder
+app.use(express.static(path.join(__dirname, 'app/public')));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json()); // parse form data client
